103ac06eceb850572afedfedf7f49af4650a2f7d
zin <juztz1n@gmail.com>
2024-03-01 15:35:15 +0100

    v0.0.1 simple functional UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,16 @@ export default function RootPage() {
             description: "Empty the compartment of your pantaloons at once.",
             hours_spent: 2,
           },
+          {
+            name: "Project Name",
+            description: "Empty the compartment of your pantaloons at once.",
+            hours_spent: 2,
+          },
+          {
+            name: "Project Name",
+            description: "Empty the compartment of your pantaloons at once.",
+            hours_spent: 2,
+          },
         ]}
       />
     </main>
@@ -36,11 +46,11 @@ function ProjectCard({ project }: { project: Project }) {
   return (
     <div className="rounded-xl bg-slate-800 text-foreground p-4 flex flex-col gap-2">
       <header>
-        <h1 className="text-2xl font-semibold">{project.name}</h1>
-        <p>{project.description}</p>
+        <h1 className="text-xl font-semibold">{project.name}</h1>
+        <p className="tracking-wide">{project.description}</p>
       </header>
       <div className="mt-2">
-        <div className="p-4 bg-slate-600 text-foreground rounded-xl w-fit">
+        <div className="p-4 bg-slate-600 text-foreground rounded-xl w-fit ">
           {project.hours_spent} hours spent
         </div>
       </div>
